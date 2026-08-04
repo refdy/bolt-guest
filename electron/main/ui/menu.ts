4212d7e59b7181862821f45ed89ab9442eebf82b
rootref <rootref@localhost>
2026-08-04 20:00:41 +0700

    feat(electron): add full application menu

--- electron/main/ui/menu.ts
+++ electron/main/ui/menu.ts
@@ -1,29 +1,92 @@
 import { BrowserWindow, Menu } from 'electron';
 
 export function setupMenu(win: BrowserWindow): void {
-  const app = Menu.getApplicationMenu();
-  Menu.setApplicationMenu(
-    Menu.buildFromTemplate([
-      ...(app ? app.items : []),
-      {
-        label: 'Go',
-        submenu: [
-          {
-            label: 'Back',
-            accelerator: 'CmdOrCtrl+[',
-            click: () => {
-              win?.webContents.navigationHistory.goBack();
-            },
+  const template: Electron.MenuItemConstructorOptions[] = [
+    {
+      label: 'File',
+      submenu: [{ role: 'close' }, { role: 'quit' }],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'selectAll' },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+      ],
+    },
+    {
+      label: 'Build',
+      submenu: [
+        {
+          label: 'Build Project',
+          click: () => win.webContents.send('menu-build'),
+        },
+      ],
+    },
+    {
+      label: 'Preview',
+      submenu: [
+        {
+          label: 'Refresh Preview',
+          accelerator: 'F5',
+          click: () => win.webContents.reload(),
+        },
+      ],
+    },
+    {
+      label: 'Go',
+      submenu: [
+        {
+          label: 'Back',
+          accelerator: 'CmdOrCtrl+[',
+          click: () => {
+            if (win.webContents.navigationHistory.canGoBack()) {
+              win.webContents.navigationHistory.goBack();
+            }
           },
-          {
-            label: 'Forward',
-            accelerator: 'CmdOrCtrl+]',
-            click: () => {
-              win?.webContents.navigationHistory.goForward();
-            },
+        },
+        {
+          label: 'Forward',
+          accelerator: 'CmdOrCtrl+]',
+          click: () => {
+            if (win.webContents.navigationHistory.canGoForward()) {
+              win.webContents.navigationHistory.goForward();
+            }
           },
-        ],
-      },
-    ]),
-  );
+        },
+      ],
+    },
+    {
+      role: 'windowMenu',
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'About',
+          click: () => {},
+        },
+      ],
+    },
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
